7dd65928c6af5c6c9138cf8d7ed1b7c4f4a8bbd5
ssshow16 <ssshow16@gmail.com>
2016-04-26 00:42:58 +0900

    enrollments api 정리

--- common/models/enrollments.js
+++ common/models/enrollments.js
@@ -1,22 +1,39 @@
 module.exports = function(Enrollments) {
-  Enrollments.disableRemoteMethod("create", true);
-  Enrollments.disableRemoteMethod("upsert", true);
+  Enrollments.disableRemoteMethod("create", false);       // PUT  /enrollments
+  Enrollments.disableRemoteMethod("upsert", false);       // POST /enrollments
   Enrollments.disableRemoteMethod("updateAll", true);
   Enrollments.disableRemoteMethod("updateAttributes", false);
-  Enrollments.disableRemoteMethod("find", true);
+  Enrollments.disableRemoteMethod('createChangeStream', true);
+
+  Enrollments.disableRemoteMethod("find", false);
   Enrollments.disableRemoteMethod("findById", true);
   Enrollments.disableRemoteMethod("findOne", true);
 
-  Enrollments.greet = function(msg, cb) {
-    console.log("greeting!!!");
+  Enrollments.disableRemoteMethod("deleteById", true);
+
+  Enrollments.disableRemoteMethod("confirm", true);
+  Enrollments.disableRemoteMethod("count", true);
+  Enrollments.disableRemoteMethod("exists", true);
+  Enrollments.disableRemoteMethod("resetPassword", true);
+
+  Enrollments.disableRemoteMethod('__count__accessTokens', false);
+  Enrollments.disableRemoteMethod('__create__accessTokens', false);
+  Enrollments.disableRemoteMethod('__delete__accessTokens', false);
+  Enrollments.disableRemoteMethod('__destroyById__accessTokens', false);
+  Enrollments.disableRemoteMethod('__findById__accessTokens', false);
+  Enrollments.disableRemoteMethod('__get__accessTokens', false);
+  Enrollments.disableRemoteMethod('__updateById__accessTokens', false);
+
+  Enrollments.start = function(msg, cb) {
+    console.log("start lesson");
     cb(null, 'Greetings... ' + msg);
   }
 
-    Enrollments.remoteMethod(
-        'greet',
-        {
-          accepts: {arg: 'msg', type: 'string'},
-          returns: {arg: 'greeting', type: 'string'}
-        }
-    );
+  Enrollments.remoteMethod(
+      'start',
+      {
+        accepts: {arg: 'msg', type: 'string'},
+        returns: {arg: 'greeting', type: 'string'}
+      }
+  );
 };
