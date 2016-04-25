72b45ee7db94c4d0ff2d60089156040053b795e5
sononpos <sononpos@gmail.com>
2016-04-25 20:16:06 +0900

    remotemethod
    test

--- common/models/enrollments.js
+++ common/models/enrollments.js
@@ -1,3 +1,22 @@
 module.exports = function(Enrollments) {
+  Enrollments.disableRemoteMethod("create", true);
+  Enrollments.disableRemoteMethod("upsert", true);
+  Enrollments.disableRemoteMethod("updateAll", true);
+  Enrollments.disableRemoteMethod("updateAttributes", false);
+  Enrollments.disableRemoteMethod("find", true);
+  Enrollments.disableRemoteMethod("findById", true);
+  Enrollments.disableRemoteMethod("findOne", true);
 
+  Enrollments.greet = function(msg, cb) {
+    console.log("greeting!!!");
+    cb(null, 'Greetings... ' + msg);
+  }
+
+    Enrollments.remoteMethod(
+        'greet',
+        {
+          accepts: {arg: 'msg', type: 'string'},
+          returns: {arg: 'greeting', type: 'string'}
+        }
+    );
 };
