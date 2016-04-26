ace3291ed6d90e17b26d8e6bec92710e3846c640
bruce.shin <ssshow16@gmail.com>
2016-04-26 16:58:41 +0900

    add enrollments.start

--- common/models/enrollments.js
+++ common/models/enrollments.js
@@ -1,6 +1,6 @@
 module.exports = function(Enrollments) {
-  Enrollments.disableRemoteMethod("create", true);       // PUT  /enrollments
-  Enrollments.disableRemoteMethod("upsert", true);       // POST /enrollments
+  Enrollments.disableRemoteMethod("create", false);       // PUT  /enrollments
+  Enrollments.disableRemoteMethod("upsert", false);       // POST /enrollments
   Enrollments.disableRemoteMethod("updateAll", true);
   Enrollments.disableRemoteMethod("updateAttributes", false);
   Enrollments.disableRemoteMethod('createChangeStream', true);
@@ -23,18 +23,51 @@ module.exports = function(Enrollments) {
   Enrollments.disableRemoteMethod('__findById__accessTokens', false);
   Enrollments.disableRemoteMethod('__get__accessTokens', false);
   Enrollments.disableRemoteMethod('__updateById__accessTokens', false);
-  //
-  // //GET /enrollments/start
-  // Enrollments.start = function(msg, cb) {
-  //   console.log("start lesson");
-  //   cb(null, 'Greetings... ' + msg);
-  // }
-  //
-  // Enrollments.remoteMethod(
-  //     'start',
-  //     {
-  //       accepts: {arg: 'msg', type: 'string'},
-  //       returns: {arg: 'greeting', type: 'string'}
-  //     }
-  // );
+
+  //GET /enrollments/start
+  Enrollments.start = function(enrollmentsId, cb) {
+    var startLesson = function(enrollment, cb){
+      enrollment.students(function(err, student){
+        if(err) throw err;
+        if(student && student.email){
+          enrollment.lessons(function(err, lesson){
+            lesson.teachers(function(err, teacher){
+              if(err) throw err;
+              if(teacher && teacher.email){
+                console.log('launch Handout teacher:' + teacher.email + ', student:' + student.email);
+                enrollment.status = 'start'
+                Enrollments.upsert(enrollment, function(err){
+                  cb(null,'OK')
+                });
+              }else{
+                cb(new Error("teachers'email must be set."),null)
+              }
+            });
+          });
+        }else{
+          cb(new Error("students'email must be set."),null)
+        }
+      });
+    }
+
+    Enrollments.findById(enrollmentsId,[], function(err, enrollment){
+      if(err) throw err;
+      if(enrollment){
+        switch (enrollment.status) {
+          case 'open': startLesson(enrollment, cb); break;
+          default: cb(null,'cannot start lesson. invalid status ' + enrollment.status)
+        }
+      } else {
+        cb(new Error('not found enrollment ' + enrollmentsId),null)
+      }
+    });
+  }
+
+  Enrollments.remoteMethod(
+      'start',
+      {
+        accepts: {arg: 'enrollmentsId', type: 'string'},
+        returns: {arg: 'result', type: 'string'}
+      }
+  );
 };
