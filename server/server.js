fe45471988777d970a4ac9124daa1e342ad4562a
sononpos <sononpos@gmail.com>
2016-04-29 00:14:25 +0900

    페이스북 로그인 추가
    http://localhost:3000/index.html
    에서 테스트 가능
    
    Students 모델에 저장

--- server/server.js
+++ server/server.js
@@ -1,7 +1,68 @@
 var loopback = require('loopback');
 var boot = require('loopback-boot');
-
 var app = module.exports = loopback();
+
+// Passport configurators..
+var loopbackPassport = require('loopback-component-passport');
+var PassportConfigurator = loopbackPassport.PassportConfigurator;
+var passportConfigurator = new PassportConfigurator(app);
+
+var bodyParser = require('body-parser');
+var flash      = require('express-flash');
+
+var config = {};
+try {
+  config = require('../providers.json');
+} catch (err) {
+  console.trace(err);
+  process.exit(1); // fatal
+}
+
+boot(app, __dirname, function(err) {
+  if (err) throw err;
+
+  // start the server if `$ node server.js`
+  if (require.main === module)
+    app.start();
+});
+
+app.middleware('parse', bodyParser.json());
+
+app.middleware('parse', bodyParser.urlencoded({
+  extended: true
+}));
+
+app.middleware('auth', loopback.token({
+  model: app.models.accessToken
+}));
+
+//app.middleware('session:before', loopback.cookieParser(app.get('cookieSecret')));
+
+app.middleware('session', loopback.session({
+  secret: 'kitty',
+  saveUninitialized: true,
+  resave: true
+}));
+passportConfigurator.init();
+
+app.use(flash());
+
+passportConfigurator.setupModels({
+  userModel: app.models.Students,
+  userIdentityModel: app.models.userIdentity,
+  userCredentialModel: app.models.userCredential
+});
+for (var s in config) {
+  var c = config[s];
+  c.session = c.session !== false;
+  passportConfigurator.configureProvider(s, c);
+}
+var ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn;
+
+app.get('/auth/account', ensureLoggedIn('/Students/login'), function (req, res, next) {
+  res.redirect('/login_ok.html');
+  console.log(req.user);
+});
 
 app.start = function() {
   // start the web server
@@ -15,13 +76,3 @@ app.start = function() {
     }
   });
 };
-
-// Bootstrap the application, configure models, datasources and middleware.
-// Sub-apps like REST API are mounted via boot scripts.
-boot(app, __dirname, function(err) {
-  if (err) throw err;
-
-  // start the server if `$ node server.js`
-  if (require.main === module)
-    app.start();
-});
