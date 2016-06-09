03a18834db90cebd1599f6e1d3518af2ac251752
bruce.shin <ssshow16@gmail.com>
2016-06-09 18:27:01 +0900

    * React Bootstrap 추가 * 파일모니터링 및 자동 빌드 스크립트 추가

--- build.js
+++ build.js
@@ -1,5 +1,7 @@
 var fs = require("fs");
 var browserify = require("browserify");
+var watchify = require('watchify');
+
 //TODO 화면별로 하나씩 만들어줘야하나?
 browserify(
   ["./client/src/student.js"]
@@ -12,3 +14,17 @@ browserify(
   ).transform("babelify", {presets: ["es2015", "react"]})
   .bundle()
   .pipe(fs.createWriteStream("./client/dist/student.js"));
+
+var b = browserify({
+  entries:["./client/src/test-react-bootstrap.js"],
+  cache: {},
+  packageCache: {},
+  plugin: [watchify]
+}).transform("babelify", {presets: ["es2015", "react"]});
+
+b.on("update",bundle);
+bundle();
+
+function bundle(){
+  b.bundle().pipe(fs.createWriteStream("./client/dist/test-react-bootstrap.js"));
+}
